35e4558cf1c4f8fccc3ba31521d8d4d777081b54
jairosl <jairo.lima@aluno.uepb.edu.br>
2024-01-23 20:43:49 -0300

    feat: add create product in IOC

--- src/DI/ioc.ts
+++ src/DI/ioc.ts
@@ -6,17 +6,24 @@ import AddProductUseCase, {
 } from '../Domain/UseCases/Cart/AddProduct.usecase';
 import ProductGateway from '../Domain/Gateways/Product';
 import CartGateway from '../Domain/Gateways/Cart';
+import CreateProductUseCase, {
+  CreateProductUseCaseType,
+} from '../Domain/UseCases/Product/CreateProduct.usecase';
 
 const container = createContainer<{
   ProductGateways: ProductGateway;
   CartGateways: CartGateway;
   AddProductsInCartUseCase: AddProductUseCaseType;
+  CreateProductUseCase: CreateProductUseCaseType; //
 }>();
 
 container.register({
+  // gateways
   ProductGateways: asFunction(GatewaysProductMemory),
   CartGateways: asFunction(GatewaysCartMemory),
+  // usecases
   AddProductsInCartUseCase: asFunction(AddProductUseCase),
+  CreateProductUseCase: asFunction(CreateProductUseCase),
 });
 
 export default container;
