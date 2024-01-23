f5b38506f3f36d933b5d358b931ef72746d3beab
jairosl <jairo.lima@aluno.uepb.edu.br>
2024-01-23 09:31:38 -0300

    styles: create color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'rgb(var(--primary) / <alpha-value>)',
+        secondary: 'rgb(var(--secondary) / <alpha-value>)',
+        tertiary: 'rgb(var(--tertiary) / <alpha-value>)',
+      },
+      textColor: {
+        black: 'rgb(var(--text-black) / <alpha-value>)',
+        label: 'rgb(var(--text-label) / <alpha-value>)',
+      },
+      borderColor: {
+        primary: 'rgb(var(--border-primary) / <alpha-value>)',
+        gray: 'rgb(var(--border-gray) / <alpha-value>)',
+        secondary: 'rgb(var(--border-secondary) / <alpha-value>)',
+      },
+      backgroundColor: {
+        'line-primary': 'rgb(var(--line-primary) / <alpha-value>)',
+        'line-secondary': 'rgb(var(--line-secondary) / <alpha-value>)',
+      },
+      placeholderColor: {
+        primary: 'rgb(var(--placeholder-primary) / <alpha-value>)',
+      },
+    },
   },
   plugins: [],
 };
-
