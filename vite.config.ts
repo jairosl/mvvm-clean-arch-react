a4ffbee13a14a9a0f2a3818d1bbe056751d805fb
jairosl <jairo.lima@aluno.uepb.edu.br>
2024-01-25 21:35:32 -0300

    test: select coverage provider

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {},
+  test: {
+    coverage: {
+      provider: 'v8',
+    },
+  },
 });
